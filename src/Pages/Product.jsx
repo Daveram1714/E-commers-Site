d95c9ce8c32b9abfb8c5e5d3ff535ef0c5f96f9c
Daveram1714 <143275788+Daveram1714@users.noreply.github.com>
2024-11-23 06:09:18 +0530

    There is an issue in the BreadCrum In showing the Image name from Productt and Item Component using useConetxt

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom'
+import Breadcrum from '../Component/Breadcrums/Breadcrum'
 
 const Product = () => {
+const {all_product} = useContext(ShopContext)
+const {productid} = useParams()
+const product = all_product.find(item => item.id === Number(productid))
+
   return (
-    <div>Product</div>
+    <div >
+      <Breadcrum  product = {product}/>
+    </div>
   )
 }
 
