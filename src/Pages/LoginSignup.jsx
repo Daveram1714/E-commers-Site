b591e14ebd987bf1debda17cb0559c50fe7215c2
Daveram1714 <143275788+Daveram1714@users.noreply.github.com>
2024-11-23 06:08:16 +0530

    Login Signup Nav BAr Issue

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import './Css/LoginSignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign-up</h1>
+        <div className="login_signupfileds">
+          <input type="text"    placeholder='your name'/>
+          <input type="email"    placeholder='your Email'/>
+          <input type="Password"    placeholder='your password'/>
+        </div>
+        <button>Contine</button>
+        <p className="loginsignup-login">Already have an Account <span>Login here</span> </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox"  name='' id=''/>
+          <label for="">I agree to the terms and conditions</label>
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
