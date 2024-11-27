faf3221d93f6dcbea654c1429f70db131b5a232d
Daveram1714 <143275788+Daveram1714@users.noreply.github.com>
2024-11-27 06:11:44 +0530

    The shop context has been Updates and The update is thta added a key to the contextValue for properly destructuring and the change in the App.js also in the Inndex.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
-import React from 'react'
-import Navbar from './Component/Navbar/Navbar'
-import { Route,Routes } from 'react-router-dom'
-import Shop from './Pages/Shop'
-import ShopCategory from './Pages/ShopCategory'
-import Product from './Pages/Product'
-import Cart from './Pages/Cart'
-import LoginSignup from './Pages/LoginSignup'
-import Missing from './Missing'
-import Footer from './Component/Footer/Footer'
-import  men_banner from './Component/Asserts/banner_mens.png'
-import womens_banner from './Component/Asserts/banner_women.png'
-import kids_banner from './Component/Asserts/banner_kids.png'
-
+import React from 'react';
+import Navbar from './Component/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import Missing from './Missing';
+import Footer from './Component/Footer/Footer';
+import men_banner from './Component/Asserts/banner_mens.png';
+import womens_banner from './Component/Asserts/banner_women.png';
+import kids_banner from './Component/Asserts/banner_kids.png';
 
 const App = () => {
-
-
-
-
-  
-  return (<div>
-    <Navbar />
-    
-    <Routes>
-      <Route path = '/' element ={<Shop/>} />
-      <Route path = 'mens' element = {<ShopCategory banner = {men_banner} category = 'men' />}/>
-      <Route path = 'womens' element = {<ShopCategory banner= {womens_banner} category = 'women'/>}/>
-      <Route path = 'kids' element = {<ShopCategory banner = {kids_banner} category = 'kids' />} />
-      <Route path = 'product' element ={<Product />} />
-      <Route path = ':id' element = {<Product />} />
-      <Route path = 'cart' element = {<Cart />} />
-      <Route path = 'login' element ={<LoginSignup />} />
-      <Route path = '*' element ={<Missing />} />
-
-    </Routes>
-    <Footer />
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="mens" element={<ShopCategory banner={men_banner} category="men" />} />
+        <Route path="womens" element={<ShopCategory banner={womens_banner} category="women" />} />
+        <Route path="kids" element={<ShopCategory banner={kids_banner} category="kids" />} />
+        <Route path="product/:productid" element={<Product />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="login" element={<LoginSignup />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,18 +1,16 @@
-import React, { createContext } from 'react'
-import all_products from "../Component/Asserts/all_product"
+import React, { createContext } from 'react';
+import all_products from '../Component/Asserts/all_product'; // Ensure this file exists and exports an array of products.
 
+export const ShopContext = createContext(null);
 
-export const ShopContext = createContext(null)
 const ShopContextProvider = (props) => {
-    const contextValue = {all_products};
+  const contextValue = { all_product: all_products }; // Corrected `all_product` key.
 
   return (
-   
-   <ShopContext.Provider value={contextValue}>
-        {props.children}
-    
-   </ShopContext.Provider>
-  )
-}
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
 
-export default ShopContextProvider
+export default ShopContextProvider;
