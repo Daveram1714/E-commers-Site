edbd4a270aa80554274afdbf0f9eeeb47e9e23a8
Daveram1714 <143275788+Daveram1714@users.noreply.github.com>
2024-12-08 12:14:07 +0530

    Created a ProductDisplayPage with the Css and A Small Update in the BreadCrum Css file

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
 import Breadcrum from '../Component/Breadcrums/Breadcrum'
+import ProductDisplay from '../Component/ProductDisplay/ProductDisplay'
 
 const Product = () => {
 const {all_product} = useContext(ShopContext)
@@ -11,6 +12,7 @@ const product = all_product.find(item => item.id === Number(productid))
   return (
     <div >
       <Breadcrum  product = {product}/>
+      <ProductDisplay  product = {product} />
     </div>
   )
 }
